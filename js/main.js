13ff8aef76a576bb0d1943b51cd62b6717e3862c
ayakhalifa2000 <90094140+ayakhalifa2000@users.noreply.github.com>
2021-09-04 13:46:19 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// var x=1;
+// do
+// {
+//     console.log(x)
+//     x++;
+// }
+// while(x<=6);
+// // 
+// var y=2;
+// while(y<6);
+// {
+//     console.log(y)
+//     x++;
+// }
+// // 
+// for(var c=0 ; c<8 ; x++)
+// {
+//     console.log(x)
+// }
+//  
+var fName="Aya-Ahmed-Ahmed-Sabry-Khalifa";
+fName = fName.join("-");
+console.log(fName);
